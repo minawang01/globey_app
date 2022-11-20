9ddc06ed9453c384fcd0f82699dcaea7a7965e64
Mina Wang <77475882+cugwc@users.noreply.github.com>
2022-11-21 01:22:03 +0800

    Added code to buttons

---
+++ app/src/main/java/APP_DESIGN_PROJECT/globeyapp/AddTripActivity.kt
@@ -0,0 +1,42 @@
+package APP_DESIGN_PROJECT.globeyapp
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.Request
+import com.android.volley.toolbox.Volley
+
+
+class AddTripActivity: AppCompatActivity() {
+
+    private var save_trip_btn: Button? = null
+    private var delete_trip_btn: Button? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_trip_expanded_page)
+    }
+
+    private fun sendMessage() {
+        val url:String = "database url string"
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.GET, url, null, {
+                response ->
+                Log.e("GlobeyApp", "response was successful")
+                switchActivity(response)
+            },
+            { error ->
+                Log.e("GlobeyApp", error.toString())
+
+            }
+        )
+        Volley.newRequestQueue(this).add(jsonObjectRequest)
+    }
+
+    private fun switchActivity(jsonObjectRequest: JsonObjectRequest) {
+        // switch activity
+    }
+
+
+}
--- app/src/main/java/APP_DESIGN_PROJECT/globeyapp/MainActivity.kt
+++ app/src/main/java/APP_DESIGN_PROJECT/globeyapp/MainActivity.kt
@@ -2,10 +2,26 @@ package APP_DESIGN_PROJECT.globeyapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.content.Intent
 
 class MainActivity : AppCompatActivity() {
+
+    private var add_trip_btn: Button? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        add_trip_btn = findViewById(R.id.add_trip_btn)
+        add_trip_btn!!.setOnClickListener {
+            Log.e("MyActivity", "Add trip button was clicked")
+            switchToAddTrip()
+        }
+
+    }
+
+    private fun switchToAddTrip() {
+        val intent = Intent(this, AddTripActivity::class.java)
+        startActivity(intent)
     }
 }
